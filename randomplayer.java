bb46c325044f2a4cbabdff6d699ff10e1dd2a30b
Yiew Jason <24051515@siswa.um.edu.my>
2026-01-12 02:21:50 +0800

    Create randomplayer.java

---
+++ randomplayer.java
@@ -0,0 +1,11 @@
+
+public Move chooseMove(GameState state) {
+    ArrayList<Move> moves = state.generatePossibleMoves();
+
+    if (moves.isEmpty()) {
+        return null; // no possible moves
+    }
+
+    Random rand = new Random();
+    return moves.get(rand.nextInt(moves.size()));
+}
