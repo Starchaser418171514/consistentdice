b9c40982f7671d95865a126528f7dff887e31886
Jayden Wan Kar Ming-2025 <111164351+Starchaser418171514@users.noreply.github.com>
2026-01-12 15:19:17 +0800

    Refactor chooseMove to handle moves using Map

--- randomplayer.java
+++ randomplayer.java
@@ -1,3 +1,10 @@
+package consistentdice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 public class RandomPlayer extends Player {
 
     private Random rand = new Random();
@@ -10,14 +17,23 @@ public class RandomPlayer extends Player {
     public int[] chooseMove(int diceValue) {
 
         // get possible moves for dice roll
-        ArrayList<int[]> possibleMoves = GameState.generatePossibleMoves(diceValue);
+        Map<Integer, List<Integer>> possibleMoves = GameState.generatePossibleMoves(diceValue);
         // each int[] is {pieceNumber, newPosition}
 
         if (possibleMoves.isEmpty()) {
             return null;
         }
 
-        int[] chosenMove = possibleMoves.get(rand.nextInt(possibleMoves.size())); //random choose a move
+        List<int[]> moveList = new ArrayList<>(possibleMoves.size());
+        for (Map.Entry<Integer, List<Integer>> entry : possibleMoves.entrySet()) {
+            int piece = entry.getKey();
+            List<Integer> moves = entry.getValue();
+            for (Integer target : moves) {
+                moveList.add(new int[]{piece, target});
+            }
+        }
+
+        int[] chosenMove = moveList.get(rand.nextInt(moveList.size())); //randomly choose a move
 
         int piece = chosenMove[0];
         int newPos = chosenMove[1];
